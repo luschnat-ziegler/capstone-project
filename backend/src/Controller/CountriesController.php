2370f379c5bd629871a51b99dd4add003c909ce9
Marian Luschnat-Ziegler <marian@luschnat.de>
2020-11-23 12:08:19 +0100

    Add fix to serializer integration

--- backend/src/Controller/CountriesController.php
+++ backend/src/Controller/CountriesController.php
@@ -22,70 +22,26 @@ class CountriesController extends AbstractController
         if (array_key_exists('id', $_GET)) {
             $country = $repository->find($_GET['id']);
             if (!is_null($country)) {
-                $jsonCountryArray = [
-                    'id' => $country->getId(),
-                    'country' =>$country->getCountry(),
-                    'region' => $country->getRegion(),
-                    'freedom' => $country->getFreedom(),
-                    'gender' => $country->getGender(),
-                    'lgbtq' => $country->getLgbtq(),
-                    'environment' => $country->getEnvironment(),
-                    'corruption' => $country->getCorruption(),
-                    'inequality' => $country->getInequality(),
-                    'total' => $country->getTotal()
-                ];
                 return new JsonResponse(
-                    json_encode($jsonCountryArray),
+                    $serializer->serialize($country, 'json'),
                     JsonResponse::HTTP_OK,
                     [],
                     true
-                );
+                );    
             } else {
                 return new JsonResponse(
                     ["error" => "ID does not exits"],
                     JsonResponse::HTTP_BAD_REQUEST
                 );
             }
-
-            // The following does not work for some reason, thus the detour via an array
-            // ################
-            // return new JsonResponse(
-            //     $serializer->serialize($country, 'json'),
-            //     JsonResponse::HTTP_OK,
-            //     [],
-            //     true
-            // );
         } else {
             $countries = $repository->findAll();
-            $jsonCountriesArray = [];
-            foreach($countries as $country) {
-                $countryArray = [
-                    'id' => $country->getId(),
-                    'country' =>$country->getCountry(),
-                    'region' => $country->getRegion(),
-                    'freedom' => $country->getFreedom(),
-                    'gender' => $country->getGender(),
-                    'lgbtq' => $country->getLgbtq(),
-                    'environment' => $country->getEnvironment(),
-                    'corruption' => $country->getCorruption(),
-                    'inequality' => $country->getInequality(),
-                    'total' => $country->getTotal()
-                ];
-                array_push($jsonCountriesArray, $countryArray); 
-            }
             return new JsonResponse(
-                json_encode($jsonCountriesArray),
+                $serializer->serialize($countries, 'json'),
                 JsonResponse::HTTP_OK,
                 [],
                 true
             );
-            // Same here: Does not work!
-            // return new JsonResponse(
-            //     $serializer->serialize($countries, 'json'),
-            //     JsonResponse::HTTP_OK,
-            //     [],
-            //     true
-            // );
         }
     }
 }
